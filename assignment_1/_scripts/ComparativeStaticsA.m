0ecfe76fbe855429ab99ec2a57228d0c54c26db6
Rob Włodarski <81385057+robertwlodarski@users.noreply.github.com>
2025-10-29 14:47:26 +0000

    Assignment 1/2

--- assignment_1/_scripts/ComparativeStaticsA.m
+++ assignment_1/_scripts/ComparativeStaticsA.m
@@ -5,15 +5,29 @@
 
 % Define & discretise the function
 Wage              = @(A) arrayfun(@(A_scalar) fnSolvePrices(pTau,pEta,pChi,pBeta,pSigmaz,pAlpha,A_scalar,pa,pr,pb,pMaxIter,pStepSize),A);
+AggregateLabSup   = @(w,T) arrayfun(@(w_scalar,T_scalar) fnAggregateLabourSupply(w_scalar,T_scalar,pTau,pEta,pChi,pBeta,pSigmaz,pa,pr,pb,pMaxIter,pStepSize),w,T);
+pAmin             = 0.5;
+pAmax             = 1.5; 
+vSpaceA           = linspace(pAmin, pAmax, 10);
+[vWageA, vTA]     = Wage(vSpaceA);
+[~,vEmpA,~]       = AggregateLabSup(vWageA,vTA);
+
+% Plot options
+Plot5.vPointsX    = [1,1.25];
+Plot5.vPointsY    = Wage(Plot5.vPointsX);
+Plot5.vPointAnn   = []
 
 
-% Plots
+% Plot
 figure(5); 
-fplot(Wage, [0.5 1.5], 'LineWidth', 2.5);
+plot(vSpaceA,vWageA, 'LineWidth', 2.5,'Color',"b");
+hold on;
+xlim([pAmin pAmax]);
 yline(w_star,'--', '$w^\star$','interpreter','latex', 'LabelVerticalAlignment','bottom','LineWidth',2.5,'FontSize',18);
 grid on;
 xlabel('A');
-xlabel('w');
+ylabel('w');
+plot(Plot5.vPointsX,Plot5.vPointsY,'r*');
 %title('Bisection: Excess supply of labour (aggregate)','fontsize',18);
 %legend('Excess supply', 'Location', 'best');
 %saveas(gcf,'_figures/excess_supply_bisection.png');
---
+++ assignment_2/Main.m
@@ -0,0 +1,10 @@
+% PhD21 Computational methods: Assignment 2
+% Rob Wlodarski
+% October 2025
+
+%% 1. Prepare the environement
+clear;
+close all;
+
+% Add paths
+addpath '_functions'; 
