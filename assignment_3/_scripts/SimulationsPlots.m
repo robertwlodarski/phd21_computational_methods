570dbb6c4eea46e1a722a852a737ffdb7af77fe1
Rob Włodarski <81385057+robertwlodarski@users.noreply.github.com>
2025-11-13 17:10:59 +0000

    Simulations updates

--- assignment_3/_scripts/SimulationsPlots.m
+++ assignment_3/_scripts/SimulationsPlots.m
@@ -11,8 +11,17 @@ plot(1:1:Parameters.pT, Plots.SimAvgWealth, 'LineWidth', 2.5);
 hold on;
 grid on;
 plot(1:1:Parameters.pT, Plots.HisAvgWalth, 'LineWidth', 2.5);
-%xline(50,'--','LineWidth',1.5,'red');
+xline(50,'--','Color','r','LineWidth',1.5);
 xlabel('$t$','interpreter','latex','FontSize',14);
 ylabel('$Ea$','interpreter','latex','FontSize',14);
 legend('Simulations','Histogram','Location','best');
 saveas(gcf,'_figures/SimulationsWealth.png');
+
+%% 2. 95th perctentile plot
+
+Plots.Sim95Wealth       = prctile(ResultsSimulation.mAssets,0.95,2);
+
+% Figure
+figure(2);
+plot(1:1:Parameters.pT, Plots.Sim95Wealth, 'LineWidth', 2.5);
+
