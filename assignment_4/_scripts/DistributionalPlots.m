b6dafc5f60e5117e3fa6b9edf1fa81801b6575e8
Rob Włodarski <81385057+robertwlodarski@users.noreply.github.com>
2025-11-19 16:27:25 +0000

    Experimenting w/ distributional plots

--- assignment_4/_scripts/DistributionalPlots.m
+++ assignment_4/_scripts/DistributionalPlots.m
@@ -1,8 +1,16 @@
 %% Plotting distributions
 
-% Find maximum index (say a
+% Find maximum index, a = 60
+Distributions.pLimitA       = sum(Grids.vGridA2<60);
+
+% Colours
+Distributions.vColours      = orderedcolors("gem");
 
 figure(11);
-bar(Grids.vGridA2,Results.mMarginalWealth(1,:),'blue')
+plot(Grids.vGridA2(1:Distributions.pLimitA),cumsum(Results.mMarginalWealth(1,1:Distributions.pLimitA)),'LineWidth',2,'Color',Distributions.vColours(1,:));
 hold on;
-bar(Grids.vGridA2,Results.mMarginalWealth(50,:),'red')
+grid on;
+plot(Grids.vGridA2(1:Distributions.pLimitA),cumsum(Results.mMarginalWealth(50,1:Distributions.pLimitA)),'LineWidth',2,'Color',Distributions.vColours(1,:),'LineStyle','--');
+plot(Grids.vGridA2(1:Distributions.pLimitA),cumsum(ResultsPE.mMarginalWealth(1,1:Distributions.pLimitA)),'LineWidth',2,'Color',Distributions.vColours(2,:));
+plot(Grids.vGridA2(1:Distributions.pLimitA),cumsum(ResultsPE.mMarginalWealth(50,1:Distributions.pLimitA)),'LineWidth',2,'Color',Distributions.vColours(2,:),'LineStyle','--');
+
