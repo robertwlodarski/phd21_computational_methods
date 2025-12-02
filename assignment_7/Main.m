281288b7858257b93028b5fbc80cae71d2565e09
Rob Włodarski <81385057+robertwlodarski@users.noreply.github.com>
2025-12-02 11:17:38 +0000

    Started assignment 7

---
+++ assignment_7/Main.m
@@ -0,0 +1,13 @@
+% PhD21 Computational methods: Assignment 7
+% Rob Włodarski
+% December 2025
+
+clear;
+clc;
+
+% Add key paths
+addpath _functions/
+addpath _scripts/
+
+%% 1. Prepare parameters
+LoadParameters;
