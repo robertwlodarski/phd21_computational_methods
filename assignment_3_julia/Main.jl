12f706072ba656aca30f4d86202d6d09110d8b00
Rob Włodarski <81385057+robertwlodarski@users.noreply.github.com>
2025-12-17 18:21:47 +0000

    Implemented a better parameter structure

--- assignment_3_julia/Main.jl
+++ assignment_3_julia/Main.jl
@@ -11,18 +11,21 @@ using Roots
 using LinearAlgebra
 using Infiltrator
 using QuadGK
+using Parameters
+using Pkg
 
 ## 2.       Load functions 
 include("_functions/InnerFunctions.jl")
 include("_functions/Functions.jl")
 
 ## 3.       Set parameters
-#           Exogeneous
-const α     = 0.36
-const σ     = 0.20
-const ρ     = 0.90
-const β     = 0.96
-const δ     = 0.08
-const A     = 1.00
-
+Base.@kwdef struct Parameters{T <: Real}
+    α::T     = 0.36
+    σ::T     = 0.20
+    ρ::T     = 0.90
+    β::T     = 0.96
+    δ::T     = 0.08
+    A::T     = 1.00
+end 
+p           = Parameters()
 
