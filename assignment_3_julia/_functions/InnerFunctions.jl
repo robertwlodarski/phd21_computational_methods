f3abaa655781a830e273243633d68a7326c7a00b
Rob Włodarski <81385057+robertwlodarski@users.noreply.github.com>
2026-01-07 18:19:22 +0000

    Added the fast optimisation for next period's wealth

--- assignment_3_julia/_functions/InnerFunctions.jl
+++ assignment_3_julia/_functions/InnerFunctions.jl
@@ -1,9 +1,30 @@
 ##          Inner functions
 
 #           Content:
-#           1.      Function 1
+#           1.      Next period's wealth optimsation 
 
 
 ##################################################
-##          1.Function 1                        ##
+##          1. Next wealth optimisation         ##
 ##################################################
+
+function        fnWealthNext(iBudget,vMinWealth,iExpValueZ,p,g)
+
+    # Unpacking 
+    @unpack vGridA1 = g
+    @unpack β       = p
+
+    ## 1.       Define the objective function
+    function    fnObjective(ap)
+        ib      = searchsortedlast(vGridA1, ap)
+        ib      = clamp(ib, 1, length(vGridA1)-1)
+        iu      = ib + 1
+        w       = (vGridA1[iu] - ap) / (vGridA1[iu] - vGridA1[ib])
+        EV      = w * iExpValueZ[ib] + (1-w) * iExpValueZ[iu]
+        return -(log(iBudget - ap) + β * EV)
+    end 
+
+    ## 2.       Optimise 
+    iRes        = optimize(fnObjective, vMinWealth, iBudget, Brent(), abs_tol = 1e-9)
+    return      Optim.minimizer(iRes) 
+end
