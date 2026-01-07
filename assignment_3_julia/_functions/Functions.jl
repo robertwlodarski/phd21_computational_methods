a110c835bf544cba8faeda08eba2cf351dc8a2be
Rob Włodarski <81385057+robertwlodarski@users.noreply.github.com>
2026-01-07 15:03:14 +0000

    Initialised function files

--- assignment_3_julia/_functions/Functions.jl
+++ assignment_3_julia/_functions/Functions.jl
@@ -1,9 +1,29 @@
 # Main functions
 
+# 1. Aiyagari (1994) general equilibrium function
+
 ##################################################
 ##          1. Aiyagari (1994) GE               ##
 ##################################################
 
-function fnSolveAiyagari1994(A)
+function fnSolveAiyagari1994(p)
+
+        ## 1.   Prepare the setting
+        # Unpack parameters
+        @unpack α = p
+
+        #       Function settings 
+        iWeightOld          = 0.95
+        iErrorGE            = 10.0
+        iTolGE              = 1e-8
+        iTolVFI             = 1e-8
+        iTolDist            = 1e-8
+        iAccInterval        = 20
+        iAccStart           = 30
+        iIterNumGE          = 1
+
+        #       Unpack grids 
+
+        #       Preallocation 
 
 end
