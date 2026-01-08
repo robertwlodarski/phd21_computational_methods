5804c9d76ee494cb2b920772f8463ff8f8486d84
Rob Włodarski <81385057+robertwlodarski@users.noreply.github.com>
2026-01-08 18:15:49 +0000

    Finished the Aiyagari (1994) GE function

--- assignment_3_julia/_functions/Functions.jl
+++ assignment_3_julia/_functions/Functions.jl
@@ -21,6 +21,8 @@ function fnSolveAiyagari1994(p,g)
         iAccInterval            = 20
         iAccStart               = 30
         iIterNumGE              = 1
+        t0                      = time()
+        pVerbose                = true
 
         #               Unpack grids 
         @unpack vGridZ, mTransitionZ, vGridA1, vGridA2 = g
@@ -31,6 +33,8 @@ function fnSolveAiyagari1994(p,g)
         mPolicyCons             = zero(mValue)
         mPolicyWealthNext       = zero(mValue)
         iCurrentDistribution    = fill(1.0 / (length(vGridA2)*length(vGridZ)),(length(vGridA2),length(vGridZ)))
+        mInterpolationConstant  = fnBasisMethodInterpolation(vGridA1,vGridA2)
+
         #               Prepare the stationary skill distribution 
         iErrorDistZ             = 10.0
         iTolDistZ               = 1e-8
@@ -54,7 +58,6 @@ function fnSolveAiyagari1994(p,g)
                 # A.    Derive the K-related terms
                 iInterest       = α * A  * (iK / vLabSupply)^(α-1)-δ
                 iWage           = (1 - α) * A * (iK / vLabSupply)^(α)
-                mExpectedValue          = mValue * mTransitionZ'
 
                 # B.    Prepare to start the VFI loop 
                 iErrorVFI       = 10.0
@@ -62,6 +65,8 @@ function fnSolveAiyagari1994(p,g)
 
                 #       START THE VFI LOOP
                 while iErrorVFI > iTolVFI
+                        #               Set the expected value
+                        mExpectedValue          = mValue * mTransitionZ'
                         
                         # C.            Start iterating over a and z
                         for iz in eachindex(vGridZ)
@@ -72,7 +77,7 @@ function fnSolveAiyagari1994(p,g)
 
                         for ia in eachindex(vGridA1)
 
-                                ## 3.   NON-ACCELERATED VERSION
+                                ## 3.                   NON-ACCELERATED VERSION
                                 if      (iIterNumVFI - 1) % iAccInterval == 0 || iIterNumVFI <= iAccStart    
                                         # A.            Current values
                                         a               = vGridA1[ia]
@@ -80,16 +85,74 @@ function fnSolveAiyagari1994(p,g)
                                         
                                         # B.            Optimisation
                                         iWealthNext     = fnWealthNext(iBudget,vMinWealth,iExpValueZ,p,g)
+                                        iWealthNext     = max(iWealthNext, vMinWealth)
+                                        iConsumption    = iBudget - iWealthNext
+                                        iValue          = fnInterpolateValueFunction(iWealthNext,iBudget,iExpValueZ,p,g)
+
+                                        # C.            Updating business
+                                        mValueNew[ia,iz]        = iValue
+                                        mPolicyCons[ia,iz]      = iConsumption
+                                        mPolicyWealthNext[ia,iz]= iWealthNext
                                 else 
+                                        #               ACCELERATED VERSION
+                                        # D.            Update based on the existing values
+                                        a               = vGridA1[ia]
+                                        iBudget         = iWage * iLabour + (1 + iInterest) * a 
+                                        iConsumption    = mPolicyCons[ia,iz]
+                                        iWealthNext     = mPolicyWealthNext[ia,iz]
+
+                                        # E.            Value function
+                                        iValue          = fnInterpolateValueFunction(iWealthNext,iBudget,iExpValueZ,p,g)
+                                        mValueNew[ia,iz]= iValue
                                 end                     
 
-                                #       ACCELERATED VERSION
+                                
                         end 
                         end 
 
+                        # 4.            Update VFI loop items
+                        iIterNumVFI     = iIterNumVFI + 1
+                        iErrorVFI       = maximum(abs.(mValue - mValueNew))
+                        mValue          .= mValueNew
                 #       CLOSE THE VFI LOOP
                 end 
+
+                ## 5.           Wealth policy: Create a more granular grid 
+                mPolicyWealthNext2      = zeros(size(iCurrentDistribution))
+                if length(vGridA1) == length(vGridA2)
+                        mPolicyWealthNext2 .= mPolicyWealthNext
+                else 
+                        mPolicyWealthNext2 = mInterpolationConstant * mPolicyWealthNext
+                end 
+
+                ## 6.           Iteration method
+                iErrorDist      = 10.0
+                while iErrorDist > iTolDist
+                        #                       A. Compute next distribution
+                        iNextDistribution       = fnIterationMethod(iCurrentDistribution,mPolicyWealthNext2,g)
+
+                        #                       B. Compute error 
+                        iErrorDist              = maximum(abs.(iNextDistribution-iCurrentDistribution))
+                        iCurrentDistribution    .= iNextDistribution
+                        # CLOSE THE INTERPOLATION LOOP
+                end 
+
+                ## 7.           Aggregation & updating business
+                iMarginalDist   = vec(sum(iCurrentDistribution,dims=2))
+                iEndoK          = dot(vGridA2,iMarginalDist)
+                iErrorGE        = abs(iEndoK - iK)
+                iK              = iWeightOld * iK + (1 - iWeightOld) * iEndoK
+
+                # 7. Print cute messages 
+                if (pVerbose && (iIterNumGE - 1) % 50 == 0) || iErrorGE < iTolGE
+                        println("SUMMARY")
+                        @printf("Maximum error:     %.3f \n", iErrorGE)
+                        @printf("Interest rate:     %.3f \n", iInterest)
+                        @printf("Wage:              %.3f \n", iWage)
+                        @printf("Elapsed time:      %.3f \n", time() - t0) 
+                end
+                iIterNumGE += 1
                 #       CLOSE THE GE LOOP
         end 
-
+return iK
 end
