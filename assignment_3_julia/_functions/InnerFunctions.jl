876316e7adab2ea78f6f1b144b23ee207aff7ae0
Rob Włodarski <81385057+robertwlodarski@users.noreply.github.com>
2026-01-08 16:20:31 +0000

    Added value function interpolation

--- assignment_3_julia/_functions/InnerFunctions.jl
+++ assignment_3_julia/_functions/InnerFunctions.jl
@@ -2,6 +2,7 @@
 
 #           Content:
 #           1.      Next period's wealth optimsation 
+#           2.      Interpolate value function
 
 
 ##################################################
@@ -28,3 +29,23 @@ function        fnWealthNext(iBudget,vMinWealth,iExpValueZ,p,g)
     iRes        = optimize(fnObjective, vMinWealth, iBudget, Brent(), abs_tol = 1e-9)
     return      Optim.minimizer(iRes) 
 end
+
+##################################################
+##          2. Interpolate value function       ##
+##################################################
+
+function        fnInterpolateValueFunction(iWealthNext,iBudget,iExpValueZ,p,g)
+    
+    # Unpacking
+    @unpack vGridA1 = g
+    @unpack β       = p
+
+    ## 1.       Get weight 
+    ib      = searchsortedlast(vGridA1, iWealthNext)
+    ib      = clamp(ib, 1, length(vGridA1)-1)
+    iu      = ib + 1
+    w       = (vGridA1[iu] - iWealthNext) / (vGridA1[iu] - vGridA1[ib])
+    EV      = w * iExpValueZ[ib] + (1-w) * iExpValueZ[iu]
+    return (log(iBudget - iWealthNext) + β * EV)
+
+end 
