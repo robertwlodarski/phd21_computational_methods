871836687bdde698ab42991a1de3ca59f12737a2
Rob Włodarski <81385057+robertwlodarski@users.noreply.github.com>
2026-04-09 13:53:08 +0100

    Executed first steps towards the backwards induction part of the algorithm

--- replication_buera_et_al_2015/scripts/FunctionsMITShock.jl
+++ replication_buera_et_al_2015/scripts/FunctionsMITShock.jl
@@ -10,4 +10,22 @@
 # A. VFI components 
 # -----------------------------------------------
 
-# 1. Update value function 
+# 1. Static policies (MIT)
+function fnStaticPoliciesMIT!(params, mit_endo, A⃗, λ⃗)
+    
+    # A. Unpacking business 
+    @unpack α, δ, θ, z⃗, λ, a⃗,Tᴹᴵᵀ = params
+    
+    for it in 1:Tᴹᴵᵀ
+        A1 = @. α * A⃗[it] * z⃗ / (mit_endo.rₜ[it] + δ)
+        A2 = θ / α * (mit_endo.rₜ[it] + δ) / mit_endo.wₜ[it]
+        A3 = A2^(θ)
+
+        # Cleaned up manual dots
+        kˣ                      = @. (A1 * A3)^(1 / (1 - α - θ))
+        @. mit_endo.𝐤[:,:,it]   = min(kˣ, λ⃗[it] * a⃗')
+        @. mit_endo.𝕀ᶜ[:,:,it]  = kˣ >= λ⃗[it] * a⃗'
+        @. mit_endo.𝐥[:,:,it]   = (θ * A⃗[it] * z⃗ * mit_endo.𝐤[:,:,it]^α / mit_endo.wₜ[it])^(1 / (1 - θ))
+        @. mit_endo.Π[:,:,it]   = A⃗[it] * z⃗ * mit_endo.𝐤[:,:,it]^α * mit_endo.𝐥[:,:,it]^θ - mit_endo.wₜ[it] * mit_endo.𝐥[:,:,it] - (mit_endo.rₜ[it] + δ) * mit_endo.𝐤[:,:,it]
+    end 
+end 
