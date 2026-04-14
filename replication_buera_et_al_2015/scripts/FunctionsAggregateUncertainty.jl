abf1ba1f0143bf267dc63a71d670b174e4256c9b
Rob Włodarski <81385057+robertwlodarski@users.noreply.github.com>
2026-04-14 16:34:32 +0100

    Began to work on the uncertainty case with the Lee (2025) method

---
+++ replication_buera_et_al_2015/scripts/FunctionsAggregateUncertainty.jl
@@ -0,0 +1,17 @@
+# A. Backward logic 
+# 1. Static policies
+# 2. Last period 
+# 3. Solve for assets 
+# 4. Backward VF loop
+
+# B. Forward simulation  
+# 1. Job destruction
+# 2. Update labour market
+# 3. Compute flows
+# 4. Distribution iteration
+# 5. Aggregate states
+
+# C. Solving 
+# 1. Convex updating for MIT shock
+# 2. Labour market residual
+# 3. Government balance 
